f72be9d922d0ab2507c30191f1b9ae9778760d78
Fakhrul Alam Siddiqei <fakhrulasa@gmail.com>
2022-12-29 21:48:40 +0600

    -registration

--- app/src/main/java/com/fakhrulasa/classroutine/MainActivity.kt
+++ app/src/main/java/com/fakhrulasa/classroutine/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.fakhrulasa.classroutine
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.fakhrulasa.classroutine.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        supportActionBar.apply {
+            this!!.title = "Hello"
+            this.setDisplayHomeAsUpEnabled(true)
+        }
+
+        auth = Firebase.auth;
+
+        binding.button.setOnClickListener {
+            binding.progressHorizontal.visibility = View.VISIBLE
+            auth.createUserWithEmailAndPassword(
+                binding.email.text.toString(),
+                binding.password.text.toString()
+            ).addOnCompleteListener(this) { task->
+                binding.progressHorizontal.visibility = View.GONE
+                if(task.isSuccessful){
+                   Toast.makeText(this,"Registered Successfully", Toast.LENGTH_LONG).show()
+               } else{
+                   Toast.makeText(this,task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+               }
+            }
+        }
+    }
+
+    override fun navigateUpTo(upIntent: Intent?): Boolean {
+        finish();
+        return super.navigateUpTo(upIntent)
     }
 }
